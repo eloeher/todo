9a7ded5ec7c6d00eb8bc510bba3690171d3479b3
loeher <loeher@umich.edu>
2020-12-13 15:30:10 -0600

    lets try this

--- app.js
+++ app.js
@@ -1,19 +1,57 @@
-function newItem() {
-	var item = document.getElementById('input').value;
+function newItem(todo, trash, id) {
+    if (trash == true){
+        return;
+    }
 	var ul = document.getElementById("list");
-	var li = document.createElement('li');
+	var li = document.createElement("li");
   li.appendChild(document.createTextNode("- "+item));
+  li.setAttribute('id', id);
   ul.appendChild(li);
-  document.getElementById('input').value="";
+  document.getElementById("input").value = "";
   li.onclick = removeItem;
 }
 
+var storage = localStorage.getItem('StorageKey');
+ //if storage is not empty/if there is something in storage 
+  if(storage !== null){ 
+    //converting JSON String -> Javascript object
+    var data = JSON.parse(storage); 
+    // load all data from the backend to the frontend. 
+    loadData(data);
+    //preparing the next todo item's index  
+    var id = data.length; 
+  }
+  else{
+      id = 0;
+      data = [];
+  }
+
 document.body.onkeyup = function(e){
       if(e.keyCode == 13){
-        newItem();
+        var todo = document.getElementById("input").value;
+        newItem(todo, false, id);
+        data.push({
+            name: todo,
+            trash: false,
+            id: id
+        });
+        localStorage.setItem("StorageKey", JSON.stringify(data));
+        id = id + 1;
       }
   }
 
 function removeItem(e) {
-  e.target.parentElement.removeChild(e.target);
+
+    element = e.target;
+    e.remove();
+
+    data[element.id].trash = true;
+    localStorage.setItem("StorageKey", JSON.stringify(data));
+  // e.target.parentElement.removeChild(e.target);
+
 }
+function loadData(array) {
+    array.forEach(function(todo) {
+      newItem(todo.name, todo.trash, todo.id);
+    });
+  }
